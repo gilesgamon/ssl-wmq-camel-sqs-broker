8cdebf82e0cc042baa0fb9ca544a4d3a7a78e423
Giles Gamon <giles@lsiarchi.consulting>
2020-03-25 10:55:18 +0000

    have moved over to ECS/ECR approach from EC2

--- docker/build.sh
+++ docker/build.sh
@@ -10,15 +10,6 @@ docker cp tempCamel:/tmp/broker-1.0-SNAPSHOT.jar .
 docker rm tempCamel
 # Now use a clean container, to build a runtime (without Maven etc)
 docker build . -f Dockerfile_camel_router_runtime -t camel_router_runtime
-# mv ./server-chain.jks ../artefacts
-# mv ./broker-1.0-SNAPSHOT.jar ../artefacts
-# To upload via s3 for user_data, things got complicated - doing via an s3 single upload should be easier (for EC2 / other deployment)
-# cp -f ibm_java_install_response_file ../artefacts
-# cp -R ../startup ../artefacts
-# cp -f safe/ibm-java-x86_64-sdk-8.0-6.6.bin ../artefacts
-# cd ..
-# rm -f artefacts.tgz
-# tar czf artefacts.tgz artefacts
 
 # cd iac/ecr
 # terraform init
